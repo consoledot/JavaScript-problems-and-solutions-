cd4196e35c1db88f22362120bbca3c08454d17f5
bimbooladele@outlook.com <abimbola.oladele@thebulb.africa>
2020-12-01 23:05:20 +0100

    solved max character problem

---
+++ maximux_characters/solition1.js
@@ -0,0 +1,23 @@
+function maxChars(chars){
+    const charMap ={}
+    let max = 0
+    let maxChar={}
+    for(let char of chars){
+        charMap[char] = charMap[char] + 1 || 1
+        for(let char in charMap){
+            if(charMap[char] > max){
+                max = charMap[char]
+                maxChar[char] = charMap[char]
+                for(let cha in maxChar){
+                    if(maxChar[cha] < max){
+                        delete maxChar[cha]
+                    }
+                }
+            }
+        }
+    }
+   
+ 
+    return maxChar
+}
+console.log(maxChars("abimbcccccbbbbbbbbbbola"))
